970e8d0021c95a0fd3629e5ef81f531a095805b7
José Cláudio <jose.c.lima.sp@gmail.com>
2024-08-01 09:04:38 -0300

    Add new info form db

--- config/conn.php
+++ config/conn.php
@@ -1,10 +1,12 @@
 <?php
-$host = 'localhost';
+$host = 'viaduct.proxy.rlwy.net';
 $dbname = 'moviehub'; 
 $user = 'root';
-$pass = '';
+$pass = 'wBaVxNkAfatEKFfDyBPWFJLtHNbDEKek';
+$port = '34940';
 
-$conn = NEW PDO("mysql:host={$host};dbname={$dbname}", $user, $pass);
+$conn = new PDO("mysql:host={$host};port={$port};dbname={$dbname}", $user, $pass);
+
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
